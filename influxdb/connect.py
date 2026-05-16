a960ce83561d1ffc079843d6eca101a9e7edfb06
Deema Wazwaz <deemanidal@ieee.org>
2026-05-16 14:44:18 +0300

    Change InfluxDB token and enhance error handling
    Updated InfluxDB connection token for security and improved error message formatting.

--- influxdb/connect.py
+++ influxdb/connect.py
@@ -1,13 +1,11 @@
-# influxdb/connect.py
 from influxdb_client import InfluxDBClient
 
 print("=" * 40)
 print("InfluxDB Connection Test")
 print("=" * 40)
 
-# InfluxDB connection details
 URL = "http://localhost:8086"
-TOKEN = "h25ii5peBP4ESZkeKu03iMhr2ZdmZ2GuDAb8Z4eAR7Q91A_n3FPOAAxq1GAzqy1SZwvE_nShAyUZZo0TJGbciA=="
+TOKEN = "token"
 ORG = "smart_city"
 BUCKET = "Smart_City_Bucket"
 
@@ -25,6 +23,6 @@ try:
     
 except Exception as e:
     print("FAILED! Could not connect to InfluxDB")
-    print("Error: {e}")
+    print(f"Error: {e}")
     print("\n Make sure you started influxdb on docker!")
     print("\ndocker start influxdb")
